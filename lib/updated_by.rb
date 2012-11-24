c84c476a2992d3c001f98b8a3cdc2fb0c5d653b6
Serhiy Tykhanskyy <tyhanskij.s@gmail.com>
2012-11-23 23:51:09 -0800

    Fixed issue with not loaded ApplicationController from main source

--- lib/updated_by.rb
+++ lib/updated_by.rb
@@ -6,17 +6,16 @@ if defined?(ActiveRecord::Base)
   ActiveRecord::Base.send :include, UpdatedBy::Updated
 end
 
-
-#FIXME corrigir essa implementacao
-# class User < ActiveRecord::Base
-#   cattr_accessor :current_user
-# end
-
-class ApplicationController < ActionController::Base
-  before_filter :set_current_user 
-
+module SetCurrentUser
   private
+
   def set_current_user
     User.current_user = current_user if defined?(current_user)
   end
+end
+
+ActiveSupport.on_load(:action_controller) do
+  include SetCurrentUser
+
+  before_filter :set_current_user 
 end
